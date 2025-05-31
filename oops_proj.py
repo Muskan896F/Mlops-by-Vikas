2e8ec3b28ff37618f1295c06332f5bf646f8ae01
Muskan Ara <muskan17ara@gmail.com>
2025-05-31 15:17:13 +0530

    updated file

--- oops_proj.py
+++ oops_proj.py
@@ -16,15 +16,43 @@ class chatbook:
         
 
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
         else:
             exit()
+
+
+    
+    def signup(self):
+        email = input("enter your email here -> ")
+        pwd = input("enter your password here -> ")
+        self.username = email
+        self.password = pwd
+        print("You have successfully signed up")
+        print("\n")
+        self.menu()
+
+
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("Please signup first by pressing 1 in the main menu")
+        else:
+            uname = input("enter your email/username here ->")
+            pwd = input("enter your password here -> ")
+            if self.username==uname and self.password==pwd:
+                print("You have successfully signed in")
+                self.loggedin = True
+            else:
+                print("please input correct credentials")
+
+        print("\n")
+        self.menu()
+
 
 
 
